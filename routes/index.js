807ad47543c115ccf9a2b79506cef4041a8a9a57
Phiiileo <emmanuelboluwajib1@gmail.com>
2020-07-14 14:55:43 +0100

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
+const fs = require('fs');
+const router = express.Router();
 
-const router = express.Router()
 
 router.get('/', (req, res) => {
     const message = JSON.stringify({
         message: "Welcome to phileo nodejs api"
     })
-    console.log("general route handler", req.baseUrl + req.path)
-    res.end(message)
+    console.log("general route handler", req.baseUrl + req.path);
+    res.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    res.end(data)
 })
 
 module.exports = router
